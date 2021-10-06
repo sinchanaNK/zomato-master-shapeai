841810c8d0e1ddc6ac79381f4ce68cf6bd86193f
sinchanaNK <sinchanank19@gmail.com>
2021-10-06 11:22:04 +0530

    add entertainment carousal

--- src/components/Entertainment/Entertinmentcard.component.jsx
+++ src/components/Entertainment/Entertinmentcard.component.jsx
@@ -1,12 +1,12 @@
 import React from "react";
 import Slider from "react-slick";
 
-const EntertainmentCard = (prop) => {
+const EntertainmentCard = (props) => {
     return(
      <>
-    <div className="w-full h-30 px-2">
+    <div className="w-full h-30- px-2">
         <img className="w-full h-full rounded-xl"
-         src={props} 
+         src={props.src} 
         alt="entertainment image"
         />
     </div>
@@ -16,7 +16,7 @@ const EntertainmentCard = (prop) => {
 };
 
 const EntertainmentCardSlider = () => {
-    const EntertainmentImages =[
+    const EntertainmentImage =[
 "https://in.bmscdn.com/discovery-catalog/collections/tr:w-800,h-800:ote-MTQ1KyBFdmVudHM%3D,otc-FFFFFF,otf-Roboto,ots-64,ox-48,oy-320,ott-b:w-300/workshops-collection-202007231330.png",
 "https://in.bmscdn.com/discovery-catalog/collections/tr:w-800,h-800:ote-MTAgRXZlbnRz,otc-FFFFFF,otf-Roboto,ots-64,ox-48,oy-320,ott-b:w-300/fitness-collection-2020081150.png",
 "https://in.bmscdn.com/discovery-catalog/collections/tr:w-800,h-800:ote-NDUrIEV2ZW50cw%3D%3D,otc-FFFFFF,otf-Roboto,ots-64,ox-48,oy-320,ott-b:w-300/kids-collection-202007220710.png",
@@ -28,25 +28,53 @@ const EntertainmentCardSlider = () => {
 ];
 
 const settings = {
-    infinity: false,
-    autoplay: false,
-    slidesToShow: 4,
-    slidesToScroll:4,
-    Initialslide: 4,
-
-};
-
-return (
-    <>
-    <Slider></Slider>
-    </>
-)
-
-const settings = {
     Infinity: false,
     autoplay:false,
     slidesToShow:4,
-    slidesToShow:4,
-    Initialslide
-}
-}
+    slidesToScroll:4,
+    InitialSlide:0, 
+    responsive : [
+        {
+        breakpoints:1024,
+        settings: {
+            slidesToShow:3,
+            slidesToScroll:2,
+            Infinit:true,
+        },
+    },
+
+    {
+    breakpoints:600,
+    settings:{
+        slidesToShow:2,
+        slidesToScroll:1,
+        InitialSlide:1,
+    },
+},
+{
+    breakpoints:480,
+    settings:{
+        slidesToShow:2,
+        slidesToScroll:1,
+        InitialSlide:1,
+    },
+   },
+    
+    ],
+};
+
+
+return (
+    <>
+   
+    <Slider {...settings}>
+        {EntertainmentImage.map((image)=> (
+             <EntertainmentCard src={image} />
+        ))}
+    </Slider>
+
+    </>
+);
+        };
+
+export default EntertainmentCardSlider;
